c3be7bd1eb0a26ffa6dba3c07f6a609c5a722e30
Hamed Ramezanian <iceage2098@gmail.com>
2014-11-27 14:01:45 +0000

    Use offical facebook api for getting total shares

--- lib/social_shares/facebook.rb
+++ lib/social_shares/facebook.rb
@@ -1,14 +1,21 @@
 module SocialShares
   class Facebook < Base
     def shares!
-      response = RestClient.get url, {:params => {:id => checked_url}}
-      JSON.parse(response)["shares"] || 0
+      response = RestClient.get url, {
+        :params => {
+          :urls => checked_url,
+          :v => '1.0',
+          :method => 'links.getStats',
+          :format => 'json'
+        }
+      }
+      JSON.parse(response)[0]["total_count"] || 0
     end
 
   private
 
     def url
-      "http://graph.facebook.com/"
+      "http://api.ak.facebook.com/restserver.php"
     end
   end
 end
--- lib/social_shares/version.rb
+++ lib/social_shares/version.rb
@@ -1,3 +1,3 @@
 module SocialShares
-  VERSION = '0.2.1'
+  VERSION = '0.2.2'
 end
