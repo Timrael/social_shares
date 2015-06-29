f425e79154ef4852c2e6a5c7db6d997a278c9877
Timur Kozmenko <timraell@gmail.com>
2015-06-29 09:26:57 +0700

    feat(facebook): use graph api instead of unrealiable rest api

--- lib/social_shares/facebook.rb
+++ lib/social_shares/facebook.rb
@@ -1,44 +1,14 @@
 module SocialShares
-  # Do not use graph API because it returns sum of
-  # real shares + likes + number of comments in one number
-  # instead of just shares.
-  #
-  # You can compare it by yourself:
-  # https://graph.facebook.com/?id=http://www.apple.com/
-  # {
-  #   id: "http://www.apple.com/",
-  #   shares: 586427,   # <--- sum of real shares + likes + number of comments
-  #   comments: 11
-  # }
-  #
-  # http://api.ak.facebook.com/restserver.php?urls=http://www.apple.com/&v=1.0&method=links.getStats&format=json
-  # [
-  #   {
-  #     url: "http://www.apple.com/",
-  #     normalized_url: "http://www.apple.com/",
-  #     share_count: 253112,  # <--- just shares
-  #     like_count: 241956,
-  #     comment_count: 91359,
-  #     total_count: 586427,
-  #     click_count: 0,
-  #     comments_fbid: "388265801869",
-  #     commentsbox_count: 11
-  #   }
-  # ]
-
   class Facebook < Base
-    URL = 'http://api.ak.facebook.com/restserver.php'
+    URL = 'https://graph.facebook.com/fql'
 
     def shares!
       response = RestClient.get(URL, {
         :params => {
-          :urls => checked_url,
-          :v => '1.0',
-          :method => 'links.getStats',
-          :format => 'json'
+          :q => "SELECT share_count FROM link_stat WHERE url='#{checked_url}'"
         }
       })
-      JSON.parse(response)[0]['share_count'] || 0
+      JSON.parse(response)['data'][0]['share_count'] || 0
     end
   end
 end
