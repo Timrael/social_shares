1bdbf5b937b3259dc8d47d98e307c371bc648b48
Timur Kozmenko <timraell@gmail.com>
2016-08-26 11:00:46 +0700

    feat(facebook): FQL is deprecated, use new Graph API instead

--- lib/social_shares/facebook.rb
+++ lib/social_shares/facebook.rb
@@ -1,15 +1,19 @@
 module SocialShares
   class Facebook < Base
-    URL = 'https://graph.facebook.com/fql'
+    URL = 'http://graph.facebook.com/v2.7/'
 
     def shares!
-      response = get(URL, {
-        params: {
-          q: "SELECT share_count FROM link_stat WHERE url='#{checked_url}'"
-        }
+      response = get(URL, params: {
+        id: checked_url,
+        fields: 'share'
       })
+      json_response = JSON.parse(response)
 
-      JSON.parse(response)['data'][0]['share_count'] || 0
+      if json_response['share']
+        json_response['share']['share_count'] || 0
+      else
+        0
+      end
     end
   end
 end
